f339b730c5041eba52318cc08ff4b79c25438fad
efreeddo <efreeddo@calpoly.edu>
2021-01-24 01:37:34 -0800

    mine

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,38 @@
 console.log("js activated 4");
 
+let images = document.getElementsByTagName("img");
 chrome.runtime.onMessage.addListener(phobia_list => {
-    console.log(phobia_list)
+    for (img of images){  
+        determineBlur(img, phobia_list)
+    }
 });
 
-let images = document.getElementsByTagName("img");
-
-// blur all image
-for (img of images){
-    img.style.filter = "blur(5px)"
-}
+const determineBlur = async (img, triggers) => {
+    try {
+        const response = await fetch('http://localhost:3000/parse', {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify({
+                src: img.src,
+                triggers: triggers
+            })
+        });
+        if (!response.ok) {
+            throw new Error('Bad response')
+        }
+        const jsonResponse = await response.json();
+        if (jsonResponse.shouldBlur) {
+            img.style.filter = "blur(10px)"
+        }
+    }
+    catch (err) {
+        throw err
+    }
+};
---
+++ backend/parser_api/app.js
@@ -0,0 +1,28 @@
+// modules =================================================
+const { json } = require('body-parser');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+
+// set up port =============================================
+const port = 3000;
+app.get('/', (req, res) => res.send('Application is running..'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// APIs - parser
+const imageService = require('./service')
+app.post('/parse', (req, res) => {
+    if (req.body.src === undefined) res.status(400).json({"Error": "Please specify img src"});
+    if (req.body.triggers === undefined) res.status(400).json({"Error": "Please specify triggers"});
+    
+    const src = req.body.src
+    const triggers = req.body.triggers
+
+    imageService.needToBlur(src, triggers).then(data => res.json({
+        shouldBlur: data,
+    }))
+    .catch(err => res.status(500).json({err: err.toString()}));
+})
