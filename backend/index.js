100a4e35025d8f577a978fec986b3ec77540aa95
Irenna Lumbuun <irennanicole1@gmail.com>
2021-01-23 18:28:09 -0800

    Make frontend communicate with backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,13 @@
-console.log("js activated 3");
+console.log("js activated 4");
+
+chrome.runtime.onMessage.addListener(phobia_list => {
+    console.log(phobia_list)
+});
 
 let images = document.getElementsByTagName("img");
+
+// pass it to parser.js
+
 // blur all image
 for (img of images){
     img.style.filter = "blur(5px)"
